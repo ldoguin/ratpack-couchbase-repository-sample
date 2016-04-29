ec439afcfac1290187de86dd135e12d64989d74e
Laurent Doguin <laurent.doguin@couchbase.com>
2016-04-29 19:15:02 +0200

    use Guice registry, use async couchbase repo

--- src/main/java/org/couchbase/devex/Application.java
+++ src/main/java/org/couchbase/devex/Application.java
@@ -1,30 +1,26 @@
 package org.couchbase.devex;
 
-import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.EntityDocument;
-import com.couchbase.client.java.repository.Repository;
+import com.couchbase.client.java.repository.AsyncRepository;
 
-import ratpack.exec.Blocking;
+import ratpack.guice.Guice;
+import ratpack.rx.RxRatpack;
 import ratpack.server.RatpackServer;
 
 public class Application {
 
 	public static void main(String... args) throws Exception {
-		RatpackServer.start(server -> server.handlers(chain -> chain.path("create", ctx -> {
-			Blocking.get(() -> {
-				EntityDocument<User> document = EntityDocument.create(new User("ldoguin", 31, "Laurent", "Doguin"));
-				Repository repo = CouchbaseCluster.create().openBucket().repository();
-				return repo.upsert(document);
-			}).then(entityDoc -> ctx.render("OK"));
-		}).path("get", ctx -> {
-			Blocking.get(() -> {
-				Repository repo = CouchbaseCluster.create().openBucket().repository();
-				EntityDocument<User> ldoguin = repo.get("ldoguin", User.class);
-				return ldoguin;
-			}).then(user -> ctx.render(user.content().toString()));
-		})
-
-		));
+		RxRatpack.initialize();
+		RatpackServer.start(server -> server.registry(Guice.registry(b -> b.module(Config.class)))
+				.handlers(chain -> chain.path("create", ctx -> {
+					EntityDocument<User> document = EntityDocument.create(new User("ldoguin", 31, "Laurent", "Doguin"));
+					AsyncRepository repo = ctx.get(AsyncRepository.class);
+					RxRatpack.promise(repo.upsert(document)).then(entityDoc -> ctx.render("OK"));
+				}).path("get", ctx -> {
+					AsyncRepository repo = ctx.get(AsyncRepository.class);
+					RxRatpack.promise(repo.get("ldoguin", User.class))
+							.then(user -> ctx.render(user.get(0).content().toString()));
+				})));
 	}
 
 }
---
+++ src/main/java/org/couchbase/devex/Config.java
@@ -0,0 +1,17 @@
+package org.couchbase.devex;
+
+import com.couchbase.client.java.AsyncBucket;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.repository.AsyncRepository;
+import com.google.inject.AbstractModule;
+
+public class Config extends AbstractModule {
+
+	protected void configure() {
+		CouchbaseCluster cc = CouchbaseCluster.create();
+		Bucket bucket = cc.openBucket();
+		bind(AsyncBucket.class).toInstance(bucket.async());
+		bind(AsyncRepository.class).toInstance(bucket.repository().async());
+	}
+}
